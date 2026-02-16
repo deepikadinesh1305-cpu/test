7142122a52239a8be2166aabbe2126b29cd7034b
Mithun Ganapathy <deepikadinesh1305@gmail.com>
2026-02-16 14:43:36 +0530

    Added shell scripts for days 1 to 3

---
+++ day1.sh
@@ -0,0 +1,7 @@
+PERSONAL INFORMATION
+Name:Deepika Dinesh
+Color:Dusky
+Height:5.1
+Martial Status:ready to commit
+Address:Vanagoor
+
---
+++ day2.sh
@@ -0,0 +1,4 @@
+EDUCATION DETAILS
+10th:93.2 Passed year:2021
+12th:80 Passed year:2023
+CGPA:9.56 passed year:2027
